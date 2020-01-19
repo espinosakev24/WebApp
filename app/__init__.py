42af2748bd84848336c2869ec4959df99b998d52
espinosakev24 <916@holbertonschool.com>
2020-01-19 18:34:17 -0500

    Adds first part of database management -> 1 error when rendering content changed

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
 import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SECRET_KEY'] = "123" #os.environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///site.db"
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,22 @@
 from flask import Flask, render_template, flash, redirect, url_for
 from app.form import RegisterForm
-from app import app
+from app import app, db, bcrypt
+from models import User
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     registerForm = RegisterForm()
     if registerForm.validate_on_submit():
-        flash('Login requested for user {}'.format(
+        print("hello {}".format(registerForm.email.data))
+        hashed_password = bcrypt.generate_password_hash(registerForm.password.data).decode('utf-8')
+        user = User(username=registerForm.username.data, email=registerForm.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('{} your account has been successfully created!'.format(
             registerForm.username.data))
         return redirect(url_for('home'))
     return render_template('register.html', title = 'Register', form=registerForm)
 
 @app.route('/home')
 def home():
-    return "Home page"
+    return "page"
